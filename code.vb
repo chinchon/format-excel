165d121ba028396472fd7e1f87c3ed79f77455ce
ChinChon Na <chinchon91@gmail.com>
2018-07-12 12:47:15 +0800

    added autofilter, freeze and wrap top rows, autofit columns

--- code.vb
+++ code.vb
@@ -1,24 +1,45 @@
 Sub prettify()
 '
 ' prettify Macro
-' make excel tables more readable quickly
+' make excel tables readable quickly
 '
 ' Keyboard Shortcut: Ctrl+q
 '
-    Cells.Select
-    Cells.EntireColumn.AutoFit
-    Range("F6").Select
-    ActiveWindow.SmallScroll ToRight:=0
-    Rows("1:1").Select
-    With Selection
-        .HorizontalAlignment = xlGeneral
-        .VerticalAlignment = xlBottom
-        .WrapText = True
-        .Orientation = 0
-        .AddIndent = False
-        .IndentLevel = 0
-        .ShrinkToFit = False
-        .ReadingOrder = xlContext
-        .MergeCells = False
-    End With
+    Application.ScreenUpdating = False
+
+    ' turn on filter
+    Rows("1:1").AutoFilter
+    
+    ' autofit columns
+    'Cells.Select
+    'Cells.EntireColumn.AutoFit
+    Call mAutoWidth
+    
+    ' wrap header row
+    Rows("1:1").WrapText = True
+    
+    ' freeze header row
+    Rows("2:2").Select
+    ActiveWindow.FreezePanes = True
+    
+    ' reset cursor to A1
+    Range("A1").Select
+
+    Application.ScreenUpdating = True
 End Sub
+
+
+' https://www.mrexcel.com/forum/excel-questions/655535-how-autofit-column-width-up-maximum-size.html#post3249648
+Sub mAutoWidth()
+    Dim mCell As Range
+    Application.ScreenUpdating = False
+
+    For Each mCell In ActiveSheet.UsedRange.Rows(1).Cells
+    mCell.EntireColumn.AutoFit
+    If mCell.EntireColumn.ColumnWidth > 40 Then _
+    mCell.EntireColumn.ColumnWidth = 40
+    Next mCell
+
+    Application.ScreenUpdating = True
+End Sub
+
